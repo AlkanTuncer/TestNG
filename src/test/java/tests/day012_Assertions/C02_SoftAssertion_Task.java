9dcc501686c99c1b49d33123505d8804bba41062
AlkanTuncer <alkantuncer53@hotmail.com>
2022-07-27 19:28:56 +0300

    TestNG - SoftAssert

---
+++ src/test/java/tests/day012_Assertions/C02_SoftAssertion_Task.java
@@ -0,0 +1,17 @@
+package tests.day012_Assertions;
+
+public class C02_SoftAssertion_Task {
+
+    // Yeni bir Class Olusturun : C03_SoftAssert
+    // 1. “http://zero.webappsecurity.com/” Adresine gidin
+    // 2. Sign in butonuna basin
+    // 3. Login kutusuna “username” yazin
+    // 4. Password kutusuna “password” yazin
+    // 5. Sign in tusuna basin
+    // 6. Only Banking Pay Bills sayfasina gidin
+    // 7. “Purchase Foreign Currency” tusuna basin
+    // 8. “Currency” drop down menusunden Eurozone’u secin
+    // 9. soft assert kullanarak "Eurozone (Euro)" secildigini test edin
+    // 10. soft assert kullanarak DropDown listesinde "Switzerland (franc)" oldugunu test edin .
+
+}
