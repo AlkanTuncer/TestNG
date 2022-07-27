87fe7f14110446318a570a161a34214c55b02b99
AlkanTuncer <alkantuncer53@hotmail.com>
2022-07-27 19:09:14 +0300

    TestNG - SoftAssert

---
+++ src/test/java/tests/day012_Assertions/C01_SoftAssert.java
@@ -0,0 +1,33 @@
+package tests.day012_Assertions;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+
+public class C01_SoftAssert {
+
+    @Test
+    public void test(){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        // - HardAssert kullandığımızda ilk test fail olursa devamındaki kodlar çalışmaz.
+        //      Assert.assertTrue(a>b);
+        //      Assert.assertTrue(a<c);
+        //      Assert.assertTrue(b>a);
+
+        SoftAssert softAssert = new SoftAssert(); // - SoftAssert kullandıgımızda ise fail olan test sonrasındaki kodlar/testler çalışmaya devam eder.
+            softAssert.assertTrue(a>b,"1.Test FAILED"); // FAILED
+            softAssert.assertTrue(a>c,"2.Test FAILED"); // FAILED
+            softAssert.assertTrue(b>a,"3.Test FAILED"); // PASSED
+            softAssert.assertTrue(c>a,"4.Test FAILED"); // PASSED
+            softAssert.assertTrue(c<a,"5.Test FAILED"); // FAILED
+
+            softAssert.assertAll();
+
+    }
+
+}
