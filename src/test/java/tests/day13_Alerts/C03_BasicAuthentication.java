243eff880a758b01554b9545131b375fe1b38729
AlkanTuncer <alkantuncer53@hotmail.com>
2022-07-28 20:45:46 +0300

    TestNG - Alerts

--- src/test/java/tests/day13_Alerts/C03_BasicAuthentication.java
+++ src/test/java/tests/day13_Alerts/C03_BasicAuthentication.java
@@ -17,7 +17,7 @@ public class C03_BasicAuthentication {
     // 1- Bir class olusturun : BasicAuthentication
     // 2- https://the-internet.herokuapp.com/basic_auth sayfasina gidin
     // 3- asagidaki yontem ve test datalarini kullanarak authentication’i yapin
-    //    Html komutu : https://username:password@URL/
+    //    Html komutu : https://username:password@URL/   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     //    Username     : admin
     //    password      : admin
     // 4- Basarili sekilde sayfaya girildigini dogrulayin
