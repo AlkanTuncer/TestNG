b0b3200bb457750d4129638751004b40f18d76f4
AlkanTuncer <alkantuncer53@hotmail.com>
2022-07-28 19:15:17 +0300

    TestNG - Alerts

--- src/test/java/tests/day011_dependsOnMethods/C01_Review.java
+++ src/test/java/tests/day011_dependsOnMethods/C01_Review.java
@@ -6,9 +6,9 @@ public class C01_Review {
     // 2. Daha fazla notasyon ile tester'larin testleri daha iyi control edebilmesine imkan verir.
     // 3. @priority ile istedigimiz testleri istedigimiz siralama ile calistirabiliriz
     // 4. Raporlar yapmamiza imkan verir
-    // 5. Xml dosya ile toplu calistirma, parallel calistirma ve cross borowsing yapmamiza imkan tanir
+    // 5. Xml dosya ile toplu calistirma, parallel calistirma ve cross browsing yapmamiza imkan tanir
     // 6. Hata mesajlarini daha detayli verir
-    // 7. softAssert ile tum testlerimizi exeption almadan calistirma imkani verir
+    // 7. softAssert ile tum testlerimizi exception almadan calistirma imkani verir
     // 8. Handle Dropdown Dropdown(acilir pencere) genelde select tagi ile olusturulur ve tag icerisinde optionlari barindirir.
     // Her acilan pencere dropdown degildir Dropdown'dan var olan option'lardan birini secmek icin 3 adim gereklidir
     // -Uzerinde calisacagimiz dropdown' locate etmek
---
+++ src/test/java/tests/day13_AlertsHandle/C01_Alerts.java
@@ -0,0 +1,72 @@
+package tests.day13_AlertsHandle;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class C01_Alerts {
+
+    // ● https://the-internet.herokuapp.com/javascript_alerts adresine gidin.
+    // ● Bir metod olusturun: acceptAlert
+    //        ○ 1. butona tıklayın, uyarıdaki OK butonuna tıklayın ve result mesajının       “You successfully clicked an alert” oldugunu test edin.
+    // ● Bir metod olusturun: dismissAlert
+    //        ○ 2. butona tıklayın, uyarıdaki Cancel butonuna tıklayın ve result mesajının       “successfuly” icermedigini test edin.
+    // ● Bir metod olusturun: sendKeysAlert
+    //        ○ 3. butona tıklayın, uyarıdaki metin kutusuna isminizi yazin, OK butonuna     tıklayın ve result mesajında isminizin görüntülendiğini doğrulayın.
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.navigate().to("https://the-internet.herokuapp.com/javascript_alerts");
+    }
+
+    @Test
+    public void acceptAlert(){
+        driver.findElement(By.xpath("//button[@onclick=\"jsAlert()\"]")).click();
+
+        driver.switchTo().alert().accept();  // Alert'daki OK butonuna tıklama.
+
+        WebElement result = driver.findElement(By.xpath("//p[@id=\"result\"]"));
+        Assert.assertEquals(result.getText(),"You successfully clicked an alert");
+    }
+
+    @Test
+    public void dismissAlert(){
+        driver.findElement(By.xpath("//button[@onclick=\"jsConfirm()\"]")).click();
+
+        driver.switchTo().alert().dismiss();  // Alert'daki Cancel butonuna tıklama.
+
+        WebElement result = driver.findElement(By.xpath("//p[@id=\"result\"]"));
+        Assert.assertFalse(result.getText().contains("successfuly"));
+    }
+
+    @Test
+    public void sendKeysAlert(){
+        driver.findElement(By.xpath("//button[@onclick=\"jsPrompt()\"]")).click();
+
+        driver.switchTo().alert().sendKeys("Alkan");
+        driver.switchTo().alert().accept();
+
+        WebElement result = driver.findElement(By.xpath("//p[@id=\"result\"]"));
+        Assert.assertTrue(result.getText().contains("Alkan"));
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+}
