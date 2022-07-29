074067f34f353f404c8fa821a3f87822026a8a16
AlkanTuncer <alkantuncer53@hotmail.com>
2022-07-29 21:29:34 +0300

    TestNG - Alerts_iFrame

---
+++ src/test/java/tests/day14_Review/C04_HomeWork.java
@@ -0,0 +1,16 @@
+package tests.day14_Review;
+
+public class C04_HomeWork {
+
+    /*
+        - https://the-internet.herokuapp.com/iframe siteye gidilsin.
+        - Text Box'a 'Dünya'nın Bütün Denizleri' yazılsın.
+        - Yazı kalınlığı 'Bold' olarak seçilsin.
+        - Yazılan yazı silinsin.
+        - 'Hollanda'nın Nüfusu 8.000.000' text'i 7 kere yazılsın.
+        - Yazılan yazı ortalansın.
+        - Elemental Selenium tıklansın.
+        - 'Sauce Labs' yazısının sonuna 'box' eklenerek konsola yazılsın.
+     */
+
+}
