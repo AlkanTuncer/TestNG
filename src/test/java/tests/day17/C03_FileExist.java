a98b3c9535eb9867c0a23ae7c6eab2c503c49fcc
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-03 20:22:10 +0300

    TestNG - FileExist

--- src/test/java/tests/day17/C03_FileExist.java
+++ src/test/java/tests/day17/C03_FileExist.java
@@ -1,11 +1,16 @@
 package tests.day17;
 
-import utilities.TestBase;
+import org.testng.annotations.Test;
 
-public class C03_FileExist extends TestBase {
+public class C03_FileExist {
 
     // Selenium ile windows uygulamalarını test edemiyoruz. Ama JAVA kullanabiliriz.
     // PC'de bir dosya olup olmadıgını kontrol etmek için JAVA kullanabilriz.
     // System.getProperty("user.home") -> PC'deki user klasörünü verir.
+
+    @Test
+    public void testFilePath(){
+
+    }
 
 }
