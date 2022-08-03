873d75dc5b799e68e1d8a5bd41da115fa6a161e8
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-03 21:53:15 +0300

    TestNG - FilesUpload

---
+++ src/test/java/tests/day17/C05_FileUpload.java
@@ -0,0 +1,32 @@
+package tests.day17;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C05_FileUpload extends TestBase {
+
+    // https://the-internet.herokuapp.com/upload adresine gidelim
+    // chooseFile butonuna basalim
+    // Yuklemek istediginiz dosyayi secelim.
+    // Upload butonuna basalim.
+    // “File Uploaded!” textinin goruntulendigini test edelim.
+
+    @Test
+    public void testFileUpload(){
+        driver.get("https://the-internet.herokuapp.com/upload");
+
+        WebElement chooseFileButton = driver.findElement(By.xpath("//input[@id=\"file-upload\"]"));
+        String dosyaPath = System.getProperty("user.home")+"\\Desktop\\Deneme1\\selenium.xlsx";
+        chooseFileButton.sendKeys(dosyaPath);
+
+        WebElement uploadButton = driver.findElement(By.xpath("//input[@id=\"file-submit\"]"));
+        uploadButton.click();
+
+        WebElement fileUploadedText = driver.findElement(By.tagName("h3"));
+        Assert.assertTrue(fileUploadedText.isDisplayed(),"File Uploaded! texti görüntülenemedi.");
+    }
+
+}
