b5be612f4d3421471c4824af6741c1f5b2327d5f
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-05 21:18:28 +0300

    TestNG - Screenshot_TASK

---
+++ src/test/java/tests/day19_/C04_ScreenshotKlasoru.java
@@ -0,0 +1,72 @@
+package tests.day19_;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+import java.io.File;
+import java.io.IOException;
+
+public class C04_ScreenshotKlasoru extends TestBase {
+
+    //  amazona gidin
+    //  3 farkli test methodu ile java, honey ve elma aratip
+    //  sonuc sayfasini screenshot'i kaydedin
+
+    @Test
+    public void testJava(){
+        driver.navigate().to("https://www.amazon.com/");
+
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("Java", Keys.ENTER);
+
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File java = new File("C:\\Users\\alkan\\IdeaProjects\\TestNG\\tScreen\\java.png");
+        File geciciJava = ts.getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(geciciJava,java);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testHoney(){
+        driver.navigate().to("https://www.amazon.com/");
+
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("Honey", Keys.ENTER);
+
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File honey = new File("C:\\Users\\alkan\\IdeaProjects\\TestNG\\tScreen\\honey.png");
+        File geciciHoney = ts.getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(geciciHoney,honey);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testElma(){
+        driver.navigate().to("https://www.amazon.com/");
+
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("Elma", Keys.ENTER);
+
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File elma = new File("C:\\Users\\alkan\\IdeaProjects\\TestNG\\tScreen\\elma.png");
+        File geciciElma = ts.getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(geciciElma,elma);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
