09d5b7b7a77280e99747b681b0dac2d43f18347c
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-08 20:21:29 +0300

    TestNG - WebTables

---
+++ src/test/java/tests/day20_JavaScriptExecutor/C03_WebTables.java
@@ -0,0 +1,27 @@
+package tests.day20_JavaScriptExecutor;
+
+import utilities.TestBase;
+
+public class C03_WebTables extends TestBase {
+
+    /*
+        <table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Age</th>
+            </tr>
+            <tr>
+                <td>Alkan</td>
+                <td>Tuncer</td>
+                <td>30</td>
+            </tr>
+            <tr>
+                <td>Caglasu</td>
+                <td>Buke</td>
+                <td>25</td>
+            </tr>
+        </table>
+     */
+
+}
