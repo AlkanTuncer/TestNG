e8f1f5bc65b5446852021c5811604a3d5f8419d8
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-08 21:59:42 +0300

    TestNG - WebTables TASK_Homework

---
+++ src/test/java/tests/day20_JavaScriptExecutor/C04_WebTables.java
@@ -0,0 +1,16 @@
+package tests.day20_JavaScriptExecutor;
+
+import utilities.TestBase;
+
+public class C04_WebTables extends TestBase {
+
+    //  1. Bir metod oluşturun: printData(int row, int column);
+    //  a. Satır(row) ve sütun(column) numarasını girdiğinizde,
+    //  printData metodu bu hücredeki(cell) veriyi yazdırmalıdır.
+    //  2. Baska bir metod daha oluşturun: printDataTest();
+    //  a. Ve bu metodu printData() methodunu cagirmak icin kullanin.
+    //  b. Örnek: printData (3,5); => 3. satır, 5. Sütundaki veriyi yazdırmalıdır.
+
+    // //table//tbody//tr[5]//td[4] -> şeklinde xpath.
+
+}
